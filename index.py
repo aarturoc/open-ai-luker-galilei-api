ec364496dbd4ddda7a20cd17e2645ec1b94403a2
aarturoc <145502500+aarturoc@users.noreply.github.com>
2024-02-06 13:35:07 -0500

    Update index.py

--- index.py
+++ index.py
@@ -126,3 +126,11 @@ async def getInfoAudio(id: str):
             raise HTTPException(status_code=404, detail="Audio fil not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    
+
+@app.get("/MongoDB/GetInfoAudio/")
+async def getInfoAudio():
+    # Fetch data from MongoDB
+    data = list(collection.find())
+    data["_id"] = int(data["_id"])
+    return [list(collection.find(document)) for document in data]
